dc0fff7fd20c9aa8d20eb550cbc9db0bb862a887
RaimondsRam <jaunrietumi@gmail.com>
2024-05-16 21:01:19 +0300

    labojums

--- 7.3.py
+++ 7.3.py
@@ -18,15 +18,18 @@
 # get_city_year(1500000, 2.5, 10000, 2000000) -> 10
 
 def get_city_year(p_start, perc, delta, p_target):
+    p_last_year = p_start
     i = 1
     while True:
         p_start = p_start + int(float(p_start * perc / 100)) + delta
-         
+        if p_last_year > p_start:
+            return -1
         if (p_start >= p_target):
             return i
         i += 1
+
         if i > 100:
-            return -1
+            return "100 years limit"
 
 p_start = int(input("iedzīvotāju skaits sākumā: "))
 perc = float(input("iedzīvotāju skaita pieagums procentuāli: "))
